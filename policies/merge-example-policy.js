2b56803c639aa522d91d3e944a1187e418caa080
Ronan Quillevere <ronan.quillevere@gmail.com>
2017-09-08 17:19:14 +0100

    improve code testability

--- policies/merge-example-policy.js
+++ policies/merge-example-policy.js
@@ -12,25 +12,19 @@ module.exports = {
             .get('http://api.chucknorris.io/jokes/random')
             .set('Content-Type', 'application/json')
             .end((err, r) => {
-                if (err || !r.ok) {
-                    console.error(`problem with request1: ${err}`);
-                    next();
-                } else {
-                    let chuckQuote = logic.extractChuckQuote(r);
+                logic.simpleErrorHandling(err, r, next);
 
-                    request
-                    .get(`http://numbersapi.com/${chuckQuote.length}`)
-                    .set('Content-Type', 'application/json')
-                    .end((err, r2) => {
-                        if (err || !r2.ok) {
-                            console.error(`problem with request2: ${err}`);
-                            next();
-                        } else {
-                            let numberQuote = logic.extractNumberQuote(r2);
-                            res.send(logic.mergeQuotes(chuckQuote,numberQuote));
-                        }
-                    });
-                }
+                const chuckQuote = logic.extractChuckQuote(r);
+
+                request
+                .get(`http://numbersapi.com/${chuckQuote.length}`)
+                .set('Content-Type', 'application/json')
+                .end((err2, r2) => {
+                    logic.simpleErrorHandling(err2, r2, next);
+
+                    const numberQuote = logic.extractNumberQuote(r2);
+                    res.send(logic.mergeQuotes(chuckQuote,numberQuote));
+                });
             });
         };
     }
--- policies/policy-logic.js
+++ policies/policy-logic.js
@@ -1,20 +1,27 @@
 exports.extractChuckQuote = function (response) {
     if (!response || !response.body || !response.body.value){
-        throw new Error ('Cannot extract Chuck Norris quote ');
+        throw new Error ('Cannot extract Chuck Norris quote');
     }
     return response.body.value;
 };
 
 exports.extractNumberQuote = function (response) {
     if (!response || !response.body || !response.body.text){
-        throw new Error ('Cannot extract number quote ');
+        throw new Error ('Cannot extract number quote');
     }
     return response.body.text;
 };
 
 exports.mergeQuotes = function (chuckQuote, numberQuote){
     return {
-        chuckQuote : chuckQuote,
+        chuckquote : chuckQuote,
         numberQuote: numberQuote
     };
+};
+
+exports.simpleErrorHandling = function(error, response, next){
+    if (error || !response.ok) {
+        console.error(`problem with request: ${err}`);
+        next();
+    }
 };
