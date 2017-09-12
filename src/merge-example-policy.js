3941a5c08a9d4d59b8e796c1c1a5e0b0a8d8efa0
Ronan Quillevere <ronan.quillevere@gmail.com>
2017-09-12 12:18:18 +0100

    simplify to debug es6

--- src/merge-example-policy.js
+++ src/merge-example-policy.js
@@ -1,31 +1,32 @@
-import 'superagent';
-import * as logic from './policy-logic';
+//import 'superagent';
+//import * as logic from './policy-logic';
 
 export default {
     name: 'merge-example',
     policy: (actionParams) => {
         return (req, res, next) => {
             // eslint-disable-next-line no-console
-            console.log('executing policy of merge-example plugin', actionParams);
+            console.log('executing policy with ES6 code of merge-example plugin', actionParams);
+            res.send('{success:true}');
 
-            request
-            .get('http://api.chucknorris.io/jokes/random')
-            .set('Content-Type', 'application/json')
-            .end((err, r) => {
-                logic.simpleErrorHandling(err, r, next);
-
-                const chuckQuote = logic.extractChuckQuote(r);
-
-                request
-                .get(`http://numbersapi.com/${chuckQuote.length}`)
-                .set('Content-Type', 'application/json')
-                .end((err2, r2) => {
-                    logic.simpleErrorHandling(err2, r2, next);
-
-                    const numberQuote = logic.extractNumberQuote(r2);
-                    res.send(logic.mergeQuotes(chuckQuote,numberQuote));
-                });
-            });
+            // request
+            // .get('http://api.chucknorris.io/jokes/random')
+            // .set('Content-Type', 'application/json')
+            // .end((err, r) => {
+            //     logic.simpleErrorHandling(err, r, next);
+            //
+            //     const chuckQuote = logic.extractChuckQuote(r);
+            //
+            //     request
+            //     .get(`http://numbersapi.com/${chuckQuote.length}`)
+            //     .set('Content-Type', 'application/json')
+            //     .end((err2, r2) => {
+            //         logic.simpleErrorHandling(err2, r2, next);
+            //
+            //         const numberQuote = logic.extractNumberQuote(r2);
+            //         res.send(logic.mergeQuotes(chuckQuote,numberQuote));
+            //     });
+            // });
         };
     }
 };
