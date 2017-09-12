c7307b216b721d8c29cf0c2357ee7ab848841e99
Ronan Quillevere <ronan.quillevere@gmail.com>
2017-09-12 15:09:54 +0100

    es6 version

--- rollup.policy.cjs.js
+++ rollup.policy.cjs.js
@@ -1,19 +1,35 @@
 import eslint from 'rollup-plugin-eslint';
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
-import uglify from 'rollup-plugin-uglify';
+//import uglify from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-re'
 
 export default {
-    entry: 'src/main.js',
-    moduleName: 'mergeExamplePolicy',
+    input: 'src/main.js',
+    output :{
+        file: 'dist/merge-example-policy.cjs.js',
+        format: 'cjs'
+    },
     plugins: [
-        resolve()
+        resolve({
+            preferBuiltins: true
+        }),
+        replace({
+         patterns: [
+           {
+             match: /formidable(\/|\\)lib/,
+             test: 'if (global.GENTLY) require = GENTLY.hijack(require);',
+             replace: '',
+           }
+         ]
+       }),
+        commonjs(),
+        json(),
         ,eslint()
         ,babel(babelrc())
         //,uglify()
-    ],
-    targets: [
-        { dest: 'dist/merge-example-policy.cjs.js', format: 'cjs' }
-      ]
+    ]
 };
--- src/merge-example-policy.js
+++ src/merge-example-policy.js
@@ -1,5 +1,5 @@
-//import 'superagent';
-//import * as logic from './policy-logic';
+import Request from 'superagent';
+import * as logic from './policy-logic';
 
 export default {
     name: 'merge-example',
@@ -7,26 +7,26 @@ export default {
         return (req, res, next) => {
             // eslint-disable-next-line no-console
             console.log('executing policy with ES6 code of merge-example plugin', actionParams);
-            res.send('{success:true}');
+            //res.send('{success:true}');
 
-            // request
-            // .get('http://api.chucknorris.io/jokes/random')
-            // .set('Content-Type', 'application/json')
-            // .end((err, r) => {
-            //     logic.simpleErrorHandling(err, r, next);
-            //
-            //     const chuckQuote = logic.extractChuckQuote(r);
-            //
-            //     request
-            //     .get(`http://numbersapi.com/${chuckQuote.length}`)
-            //     .set('Content-Type', 'application/json')
-            //     .end((err2, r2) => {
-            //         logic.simpleErrorHandling(err2, r2, next);
-            //
-            //         const numberQuote = logic.extractNumberQuote(r2);
-            //         res.send(logic.mergeQuotes(chuckQuote,numberQuote));
-            //     });
-            // });
+            Request
+            .get('http://api.chucknorris.io/jokes/random')
+            .set('Content-Type', 'application/json')
+            .end((err, r) => {
+                logic.simpleErrorHandling(err, r, next);
+
+                const chuckQuote = logic.extractChuckQuote(r);
+
+                Request
+                .get(`http://numbersapi.com/${chuckQuote.length}`)
+                .set('Content-Type', 'application/json')
+                .end((err2, r2) => {
+                    logic.simpleErrorHandling(err2, r2, next);
+
+                    const numberQuote = logic.extractNumberQuote(r2);
+                    res.send(logic.mergeQuotes(chuckQuote,numberQuote));
+                });
+            });
         };
     }
 };
